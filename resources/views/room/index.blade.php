6f81736d3cf53583b17bf59b808c8e9efdd48ade
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-05-06 14:35:15 +0700

    2.3

--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -3,18 +3,29 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+            
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <h4>Data Ruangan</h4>
+                    <span>Data semua ruangan.</span>
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                                <th>Nama Ruangan</th>
+                                <th>Action</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Ruangan A</td>
+                                    <td>
+                                        <a href="#" class="btn btn-primary">Detail</a>
+                                        <a href="#" class="btn btn-danger">Hapus</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
