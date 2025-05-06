cf18eb00678d7c8247cc49552808575a878afcf0
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-05-06 14:20:13 +0700

    2.2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoomController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +11,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// Routing untuk fitur ruangan.
+Route::resource('room', RoomController::class);
+
