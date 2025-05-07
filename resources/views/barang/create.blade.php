42df49af49af4e44bdef2d8da73b7cae780c0c4e
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-05-07 11:11:53 +0700

    3.9

--- resources/views/barang/create.blade.php
+++ resources/views/barang/create.blade.php
@@ -10,33 +10,32 @@
                     {{-- area header --}}
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="">
-                            <h4>Data Ruangan</h4>
-                            <span>Form menambahkan ruangan.</span>
+                            <h4>Data Barang</h4>
+                            <span>Form menambahkan barang.</span>
                         </div>
                     </div>
                     {{-- form input --}}
-                    <form action="{{route('room.store')}}" method="post">
+                    <form action="#" method="post">
                         @csrf
                         <div class="form-group mt-2">
-                            <label for="">Nama Ruangan</label>
-                            <input type="text" name="nama_ruangan" id="nama_ruangan" required class="form-control" placeholder="ex. garasi">
+                            <label for="">Nama Barang</label>
+                            <input type="text" name="nama_barang" id="nama_barang" required class="form-control" placeholder="ex. laptop">
                         </div>
 
                         <div class="form-group mt-2">
-                            <label for="">Nomor Ruangan</label>
-                            <input type="text" name="nomor_ruangan" id="nomor_ruangan" required class="form-control" placeholder="ex. garasi">
+                            <label for="">Brand</label>
+                            <input type="text" name="brand" id="brand" required class="form-control" placeholder="ex. Macbook pro M1">
                         </div>
 
                         <div class="form-group mt-2">
-                            <label for="">Ukuran</label>
-                            <select name="ukuran" id="ukuran" required class="form-control">
+                            <label for="">Disimpan di</label>
+                            <select name="id_room" id="id_room" required class="form-control">
                                 <option value="">-Pilih Ukuran-</option>
-                                <option value="small">small</option>
-                                <option value="large">large</option>
-                                <option value="extra large">extra large</option>
+                                @foreach ($room as $iten)
+                                    <option value="{{$item->id}}">{{$item->nama_ruangan}}</option>
+                                @endforeach
                             </select>
                         </div>
-
                         <div class="form-group mt-2">
                             <button type="submit" class="btn btn-success">Tambah</button>
                         </div>
